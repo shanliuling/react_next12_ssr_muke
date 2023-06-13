943280e5e25c7d6a3a7534e6b3b3763218a4990b
zhangjinlin <1362913430@qq.com>
2023-06-13 23:39:19 +0800

    ✨ feat(登录):
    验证码平台对接

--- pages/api/user/sendVerifyCode.ts
+++ pages/api/user/sendVerifyCode.ts
@@ -1,12 +1,43 @@
-//为什么这里不能用return
-//因为这里是一个异步函数，return只能返回一个promise对象，而不是一个对象
-//如果要返回一个对象，需要用到await
-//如果不用async，那么就需要用到promise
-
-export default async function name(req, res) {
+import { format } from 'date-fns'
+import md5 from 'md5'
+import { NextApiRequest, NextApiResponse } from 'next'
+import { encode } from 'js-base64'
+import request from 'service/fetch'
+export default async function sendVerifyCode(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   const { to = '', templateId = '1' } = req.body
-  console.log('to', to)
-  console.log('templateId', templateId)
+  const AccountId = '2c94811c8853194e0188b523e0262144'
+  const AuthToken = '50340f9fc2204058a983074536dba1d9'
+  const appId = '2c94811c8853194e0188b523e17f214b'
+  const NowDate = format(new Date(), 'yyyyMMddHHmmss')
+  const SigParameter = md5(AccountId + AuthToken + NowDate)
+  const Authorization = encode(`${AccountId}:${NowDate}`)
+  const verifyCode = Math.floor(Math.random() * (9999 - 1000) + 1000) //4位数验证码
+  const expireMiunte = 5 //过期时间
+  // 业务url
+  const url = `https://app.cloopen.com:8883/2013-12-26/Accounts/${AccountId}/SMS/TemplateSMS?sig=${SigParameter}`
+  console.log(NowDate)
+  console.log(SigParameter)
+  console.log(Authorization)
+  // 给第三方发送请求 具体看文档 http://doc.yuntongxun.com/pe/5a533de33b8496dd00dce07c
+  const response = await request.post(
+    url,
+    {
+      to,
+      templateId,
+      appId,
+      datas: [verifyCode, expireMiunte],
+    },
+    {
+      headers: {
+        Authorization,
+      },
+    }
+  )
+  console.log(response)
+
   res.status(200).json({
     code: 0,
     data: 123,
